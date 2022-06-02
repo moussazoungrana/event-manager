a89dd90f2f472fec7469a32ef59f5ad9cc709424
Moussa Zoungrana <moussawalidzoungrana@gmail.com>
2022-06-02 22:26:27 +0100

    emitter class

---
+++ readme.md
@@ -0,0 +1,40 @@
+## Installation
+
+You can install the package via composer:
+
+```bash
+composer require moussazoungrana/event-manager
+```
+
+## Usage
+
+```php 
+<?php 
+
+ require __DIR__.'/../vendor/autoload.php';
+
+ \moz\EventManager\Emitter::getInstance()->on('first.event', function (){
+     echo "first event \n";
+ });
+ 
+ \moz\EventManager\Emitter::getInstance()->emit('first.event');
+ 
+ #first event
+ 
+ 
+ ###########################
+ 
+ \moz\EventManager\Emitter::getInstance()->on('second-event', function ($t){
+     echo "second event here \n";
+     echo $t;
+ });
+ 
+ \moz\EventManager\Emitter::getInstance()->emit('second.event', 'I am argument');
+  #second event here 
+  #I am argument
+ 
+ 
+ 
+ 
+```
+ 
--- src/Emitter.php
+++ src/Emitter.php
@@ -1,8 +1,61 @@
 <?php
+
 namespace moz\EventManager;
 
 class Emitter
 {
+    /**
+     * @var Emitter
+     */
+    private static $instance;
+
+
+    /**
+     * @var array
+     */
+    protected $listeners = [];
+
+    private function __construct()
+    {
+
+    }
+
+
+    /**
+     * @return static
+     */
+    public static function getInstance(): self
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new static();
+        }
+        return self::$instance;
+
+    }
+
+    /**
+     * @param string $event
+     * @param callable $listener
+     * @return void
+     */
+    public function on(string $event, callable $listener)
+    {
+        $this->listeners[$event][] = $listener;
+    }
+
+
+    /**
+     * @param string $event
+     * @param ...$args
+     * @return void
+     */
+    public function emit(string $event, ...$args)
+    {
+        $listeners = $this->listeners[$event];
+        foreach ($listeners as $listener){
+            $listener(...$args);
+        }
+    }
 
 
 }
